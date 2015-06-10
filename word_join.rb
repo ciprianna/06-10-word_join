de4e0a38f0be74286edd49899a6cb3c0a73ae5ab
Ciprianna <ciprianna17@gmail.com>
2015-06-10 12:16:14 -0500

    Word Connector program using active_support

--- word_join.rb
+++ word_join.rb
@@ -1,1 +1,9 @@
-# 
+# Word Connector
+require "active_support/core_ext/array/conversions"
+
+puts "Give me a list of words (comma separated):"
+list = gets.chomp
+
+list_array = list.split(', ')
+
+puts list_array.to_sentence
